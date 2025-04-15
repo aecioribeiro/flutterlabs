95e892d34006c308557cc333a4d19ca5c19e4028
Aecio Lima <aeclean@gmail.com>
2025-04-15 16:16:04 -0300

    add main page

--- lib/src/screens/home_page.dart
+++ lib/src/screens/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutterlabs/src/routes/app_routes.dart';
+import 'package:go_router/go_router.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,7 +8,21 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(22.0),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: ElevatedButton(
+                onPressed: () => context.push(path(AppRoutes.animations)),
+                child: const Text("Animations"),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
